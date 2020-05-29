324d1e0f6f15d2c1ec81cbee0b15d7d296a6d038
alex <alextotalk@gmail.com>
2020-05-29 12:15:37 +0300

    Updates

---
+++ precache-manifest.08972fdcf1ab2386a14a733821455775.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7315936d1f0c9edab05409e2a6ce92f3",
+    "url": "/cosial-network/index.html"
+  },
+  {
+    "revision": "224153452f58f6652d43",
+    "url": "/cosial-network/static/css/main.a4e3f3fe.chunk.css"
+  },
+  {
+    "revision": "e994a3deff0e4c42e791",
+    "url": "/cosial-network/static/js/2.18c7c3c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cosial-network/static/js/2.18c7c3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224153452f58f6652d43",
+    "url": "/cosial-network/static/js/main.829a0a1b.chunk.js"
+  },
+  {
+    "revision": "f72dba3d63231e80c308",
+    "url": "/cosial-network/static/js/runtime-main.d71a1334.js"
+  },
+  {
+    "revision": "b9b0490ac3b2b1447d3636505160dd8a",
+    "url": "/cosial-network/static/media/avatarUser.b9b0490a.png"
+  }
+]);
